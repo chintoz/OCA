366c144647b26e53ea2f5580e8d92ff39844d64e
jjmena80 <B7bgbzIu>
2017-09-17 01:20:37 +0200

    Initial version chapter 4 freemind chart

---
+++ summaries/Chapter4 - Methods and Encapsulation.mm
@@ -0,0 +1,46 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1505602944105" ID="ID_610278600" MODIFIED="1505603309898" TEXT="Designing Methods">
+<node CREATED="1505603160042" ID="ID_888231542" MODIFIED="1505603376287" POSITION="right" TEXT="Method signature">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &lt;access modifier&gt; &lt;optional specifier&gt; &lt;return type&gt; &lt;method name&gt; ( &lt;list of parameters&gt;&#160;&#160;) &lt;exception optional&gt;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;{ &lt;method body&gt; }
+    </p>
+    <p>
+      public&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;final&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;void&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160; &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;nap&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;(&#160;&#160;int minutes&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;) throws InterruptedException&#160;&#160;&#160;&#160;&#160;{ // body&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;}
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1505603396920" ID="ID_1943685719" MODIFIED="1505603945735" POSITION="left" TEXT="Access Modifiers">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>public</b>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;--&gt; Can be called by any class
+    </p>
+    <p>
+      <b>private</b>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;--&gt; Can be called only by the same class
+    </p>
+    <p>
+      <b>protected</b>&#160;&#160;&#160;&#160;--&gt; Can be called from classes in the same package or subclasses
+    </p>
+    <p>
+      <b>default</b>&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;--&gt; Only can be called from classes in the same package&#160;&#160;(not by it subclasses). There isn't a modifier. Whe te access modifier is empty then is this acces modifier.
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;There is a default key word .. but it doesn't work as an Access Modifier
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
